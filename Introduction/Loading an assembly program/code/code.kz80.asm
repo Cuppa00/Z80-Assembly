d2ede02da78634fb426900a9804b83456eb1abe5
Cuppa00 <callumalanmonk@gmail.com>
2025-12-15 17:11:00 +0000

    Update code.kz80.asm

--- Introduction/Loading an assembly program/code/code.kz80.asm
+++ Introduction/Loading an assembly program/code/code.kz80.asm
@@ -1,2 +1,4 @@
-ld bc, 99
-ret
+ld bc, 99 ; Load the value 99 into register BC
+          ; B = high byte (0), C = low byte (99)
+ret       ; Return from subroutine
+          ; When called via PRINT USR, the value in BC is returned to BASIC
